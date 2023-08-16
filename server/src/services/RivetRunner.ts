a0c300df4f34d81625cd15e9de2e6c9edded5f2a
Cai GoGwilt <cai@ironcladhq.com>
2023-08-16 15:12:10 -0700

    Move rivet-project to top level

--- server/src/services/RivetRunner.ts
+++ server/src/services/RivetRunner.ts
@@ -14,7 +14,7 @@ export async function runMessageGraph(input: { type: 'assistant' | 'user'; messa
 }
 
 export async function runRivetGraph(graphId: GraphId, inputs?: GraphInputs): Promise<GraphOutputs> {
-  const project = currentDebuggerState.uploadedProject ?? await loadProjectFromFile('src/rivet/chat.rivet-project');
+  const project = currentDebuggerState.uploadedProject ?? await loadProjectFromFile('../chat.rivet-project');
 
   const outputs = await runGraph(project, {
     graph: graphId,
