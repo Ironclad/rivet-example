c5163350750283b4aa1422c30dd5c76cb3ab6e63
Cai GoGwilt <cai@ironcladhq.com>
2023-08-16 15:27:48 -0700

    Ditch some server baggage

--- server/build.ts
+++ server/build.ts
@@ -14,9 +14,6 @@ import childProcess from 'child_process';
   try {
     // Remove current build
     await remove('./dist/');
-    // Copy front-end files
-    await copy('./src/public', './dist/public');
-    await copy('./src/views', './dist/views');
     // Copy back-end files
     await exec('tsc --build tsconfig.prod.json', './');
   } catch (err) {
--- server/src/server.ts
+++ server/src/server.ts
@@ -65,27 +65,6 @@ app.use((
 });
 
 
-// ** Front-End Content ** //
-
-// Set views directory (html)
-const viewsDir = path.join(__dirname, 'views');
-app.set('views', viewsDir);
-
-// Set static directory (js and css).
-const staticDir = path.join(__dirname, 'public');
-app.use(express.static(staticDir));
-
-// Nav to users pg by default
-app.get('/', (_: Request, res: Response) => {
-  return res.redirect('/users');
-});
-
-// Redirect to login if not logged in.
-app.get('/users', (_: Request, res: Response) => {
-  return res.sendFile('users.html', { root: viewsDir });
-});
-
-
 // **** Export default **** //
 
 export default app;
