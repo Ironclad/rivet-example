f092714c6bf54e0d66db104736a12e5921e2600a
Cai GoGwilt <cai@ironcladhq.com>
2023-11-11 10:38:06 -0800

    Make the UI look more interesting

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,9 @@
-import './App.css';
 import ChatMessages from './ChatMessages';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Debugger URL: <pre>ws://localhost:3000/api/rivet/debugger</pre></p>
-      </header>
-      <section className="App-section">
-        <ChatMessages />
-      </section>
+    <div className="App" style={{ maxWidth: '600px', margin: '0 auto' }}>
+      <ChatMessages />
     </div>
   );
 }
--- app/src/ChatMessages.tsx
+++ app/src/ChatMessages.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { FaPaperPlane, FaUserCircle, FaRobot, FaTimes } from 'react-icons/fa';
 
 interface Message {
   message: string;
@@ -13,6 +14,11 @@ const initialMessages: Message[] = [
 const Chatbot: React.FC = () => {
   const [messages, setMessages] = useState<Message[]>(initialMessages);
   const [inputValue, setInputValue] = useState("");
+  const [showHeader, setShowHeader] = useState(true);
+
+  const handleCloseHeader = () => {
+    setShowHeader(false);
+  };
 
   const handleMessageSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -27,17 +33,31 @@ const Chatbot: React.FC = () => {
   };
 
   return (
-    <div>
-      <div>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'stretch', height: '100vh' }}>
+      {showHeader && (
+        <header style={{ backgroundColor: '#333', color: '#fff', display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '8px 16px', marginBottom: '16px' }}>
+          <p>Rivet Debugger URL: <pre style={{ display: 'inline', margin: '0' }}>ws://localhost:3000/api/rivet/debugger</pre></p>
+          <button onClick={handleCloseHeader} style={{ backgroundColor: 'transparent', border: 'none', color: '#fff', cursor: 'pointer' }}><FaTimes /></button>
+        </header>
+      )}
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'stretch', flex: 1, overflowY: 'scroll', height: `calc(100vh - ${showHeader ? '56px' : '16px'})`, marginBottom: '16px' }}>
         {messages.map((message, index) => (
-          <div key={index} style={{ textAlign: message.type === 'user' ? "right" : "left" }}>
-            <span style={{ display: "inline-block", backgroundColor: message.type === 'user' ? "#4CAF50" : "#ddd", padding: "8px 12px", borderRadius: "16px", maxWidth: "70%", marginBottom: "8px", whiteSpace: "pre-wrap" }}>{message.message}</span>
+          <div key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '16px' }}>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '40px', height: '40px', borderRadius: '50%', backgroundColor: '#ddd', marginRight: '16px' }}>
+              {message.type === 'user' ? <FaUserCircle size={24} /> : <FaRobot size={24} />}
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'column', maxWidth: '100%' }}>
+              <span style={{ backgroundColor: message.type === 'user' ? '#ddd' : '#4CAF50', padding: '8px 12px', borderRadius: '16px', marginBottom: '8px', color: message.type === 'user' ? '#000' : '#fff', maxWidth: '100%', wordWrap: 'break-word', textAlign: 'left', whiteSpace: 'pre-wrap' }}>{message.message}</span>
+            </div>
           </div>
         ))}
       </div>
-      <form onSubmit={handleMessageSubmit} style={{ marginTop: "16px" }}>
-        <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-        <button type="submit">Send</button>
+      <form onSubmit={handleMessageSubmit} style={{ display: 'flex', alignItems: 'center', marginTop: '16px', marginBottom: '16px' }}>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '40px', height: '40px', borderRadius: '50%', backgroundColor: '#ddd', marginRight: '16px' }}>
+          <FaUserCircle size={24} />
+        </div>
+        <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} style={{ flex: 1, padding: '8px 12px', borderRadius: '16px', marginRight: '16px' }} />
+        <button type="submit" style={{ backgroundColor: '#4CAF50', color: '#fff', border: 'none', padding: '8px 12px', borderRadius: '16px', minWidth: '80px' }}><FaPaperPlane /></button>
       </form>
     </div>
   );
